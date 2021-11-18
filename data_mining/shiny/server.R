588be4b85fb2afb6c4044057be93f5337241ac12
Hubert Cadieux <hubertcadieux@gmail.com>
2021-11-18 01:51:19 -0500

    datamining

--- data_mining/shiny/server.R
+++ data_mining/shiny/server.R
@@ -1,11 +1,73 @@
 library(shiny)
+library(shinyWidgets)
 library(tidyverse)
+library(utils)
 
-server <- function(session, input, output) {
+server <- function(session, input, output){
+  Data <- reactive({
+    req(input$dataFile, file.exists(input$dataFile$datapath))
+    readRDS(input$dataFile$datapath)
+  })
   observe({
-    Data <- data.frame(reactive(input$dataFile))
-    updateVarSelectInput(session = session,
-                         "select_vars", "Sélectionner les variables à explorer",
-                         data = Data)
-    })
+    updatePickerInput(session = session,
+                      "select_vars", "Sélectionner les variables à explorer",
+                      choices = names(Data()))
+  })
+  observe({
+    updatePickerInput(session = session,
+                      "select_vars_fill", "Sélectionner les variables à explorer (fill)",
+                      choices = names(Data()))
+  })
+  output$wd <- renderText(getwd())
+  observeEvent(input$dl_corrs, {
+    vars <- input$select_vars
+    comb <- combn(vars, 2, simplify = F)
+    for (i in 1:length(comb)){
+      varx <- comb[[i]][1]
+      vary <- comb[[i]][2]
+      plot <- ggplot(Data(), aes_string(x = varx, y = vary)) +
+                geom_point() +
+                geom_smooth()
+      ggsave(paste0(getwd(), input$path, "/", varx, "-", vary, ".png"),
+             plot = plot)
+    }
+  }, ignoreInit = TRUE)
+  observeEvent(input$dl_histo, {
+    vars <- input$select_vars
+    for (i in 1:length(vars)){
+      var <- vars[i]
+      plot <- ggplot(Data(), aes_string(x = var)) +
+        geom_histogram()
+      ggsave(paste0(getwd(), input$path, "/", var, ".png"),
+             plot = plot)
+    }
+  }, ignoreInit = TRUE)
+  observeEvent(input$dl_densite, {
+    vars <- input$select_vars
+    for (i in 1:length(vars)){
+      var <- vars[i]
+      plot <- ggplot(Data(), aes_string(x = var)) +
+        geom_density()
+      ggsave(paste0(getwd(), input$path, "/", var, ".png"),
+             plot = plot)
+    }
+  }, ignoreInit = TRUE)
+  observeEvent(input$dl_bars, {
+    vars <- input$select_vars
+    fill <- input$select_vars_fill
+    dfvars <- expand.grid(vars, fill)
+    dfvars$Var1 <- as.character(dfvars$Var1)
+    dfvars$Var2 <- as.character(dfvars$Var2)
+    for (i in 1:nrow(dfvars)){
+      var <- dfvars[i,]$Var1
+      varfill <- dfvars[i,]$Var2
+      var <- sym(var)
+      #varfill <- sym(varfill)
+      plot <- ggplot(Data(), aes(x = !!var)) +
+                geom_bar(stat="count", aes(fill = .data[[varfill]]),
+                         position = position_dodge())
+      ggsave(paste0(getwd(), input$path, "/", var, "-", varfill, ".png"),
+             plot = plot)
+    }
+  }, ignoreInit = TRUE)
 }
--- data_mining/shiny/ui.R
+++ data_mining/shiny/ui.R
@@ -1,13 +1,25 @@
 library(shiny)
+library(shinyWidgets)
 library(shinydashboard)
 library(tidyverse)
 
 ui <- fluidPage(
-  fileInput("dataFile", "Sélectionner le fichier de données",
-            accept = c(".csv", ".xlsx", ".rds"),
+  fileInput("dataFile", "Sélectionner le fichier RData (rds)",
+            accept = c(".rds"),
             buttonLabel = "Cliquer ici",
             placeholder = "Aucun fichier sélectionné"),
-  varSelectInput("select_vars", "Sélectionner les variables à explorer",
-                 data = data.frame(),
-                 multiple = T)
+  pickerInput("select_vars", "Sélectionner les variables à explorer",
+              choices = NULL,
+              multiple = T,
+              options = list(`actions-box` = T)),
+  pickerInput("select_vars_fill", "Sélectionner les variables à explorer (fill)",
+              choices = NULL,
+              multiple = T,
+              options = list(`actions-box` = T)),
+  textOutput("wd"),
+  textInput("path", "Path"),
+  fluidRow(actionButton("dl_corrs", "Downloader nuages de points"),
+           actionButton("dl_histo", "Downloader histogrammes"),
+           actionButton("dl_densite", "Downloader densité"),
+           actionButton("dl_bars", "Downloader bar-graphs"))
 )

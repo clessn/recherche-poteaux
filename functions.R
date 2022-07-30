381c33e1f45cd6e754e1dfe1972c803c78efa4e1
Hubert Cadieux <hubert.cadieux.1@ulaval.ca>
2022-07-30 09:13:48 -0400

    merge conflict

--- functions.R
+++ functions.R
@@ -1,37 +1,37 @@
 library(tidyverse)
+library(haven)
 
-generate_rri <- function(party, ){
-    ncols <- length(names(prepped_data))
-    StepOne <- prepped_data %>%
-      pivot_longer(., all_of(2:ncols),
-                   names_to = "party",
-                   values_to = "potGrowth")
-    Max <- StepOne %>%
-      group_by(!!sym(id_col)) %>%
-      summarise(first = max(potGrowth))
-    FirstSecond <- left_join(StepOne, Max, by = id_col) %>%
-      mutate(ismax = ifelse(potGrowth == first, 1, 0)) %>%
-      group_by(!!sym(id_col)) %>%
-      mutate(nismax = sum(ismax)) %>%
-      filter(potGrowth != first) %>%
-      group_by(!!sym(id_col)) %>%
-      mutate(second = ifelse(nismax > 1, first, max(potGrowth))) %>%
-      group_by(!!sym(id_col)) %>%
-      summarise(first = unique(first),
-                second = unique(second)) %>%
-      select(id_col, first, second)
-    ThData <- left_join(StepOne, FirstSecond, by = id_col) %>%
-      mutate(irc = ifelse(potGrowth == first, potGrowth - second,
-                          potGrowth - first)) %>%
-      pivot_wider(., id_cols = !!sym(id_col), 
-                  names_from = party,
-                  values_from = irc)
-    if (!(is.na(former_prefix))){
-      names(ThData) <- gsub(former_prefix, new_prefix, names(ThData))
-    } else {
-      names(ThData) <- names(ThData)
-    }
-    return(ThData)
-  }
-  
+normalize <- function(vector){
+  min <- min(vector)
+  max <- max(vector)
+  vec <- (vector-min)/(max-min)
+  return(vec)
 }
+
+regress <-
+  function(data,
+           y,
+           x,
+           reg_type,
+           controls = as.character(),
+           family = NA) {
+    x_express <- paste0(x, collapse = " + ")
+    vars_express <- paste0(y, " ~ ", x_express)
+    controls_express <- paste0(controls, collapse = " + ")
+    varsWcont_express <- if (length(controls) == 0) {
+      vars_express
+    }
+    else {
+      paste0(vars_express, " + ", controls_express)
+    }
+    express <-
+      paste0(reg_type, "(", varsWcont_express, ", data = ", data)
+    express <- if (is.na(family)) {
+      paste0(express, ")")
+    }
+    else {
+      paste0(express, ", family = ", family, ")")
+    }
+    model <- eval(parse(text = express))
+    return(model)
+  }
